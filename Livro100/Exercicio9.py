8417b30508d203c9c91848f0cc9281e92244480d
GBJ008 <gabrielerik140@gmail.com>
2024-12-12 21:31:31 -0300

    Livro100/Exercicio9.py

--- Livro100/Exercicio9.py
+++ Livro100/Exercicio9.py
@@ -1,3 +1,3 @@
 nomes = ['Ana','Carlos','Daiane','Fernando','Maria']
-#  O colchetes serve para mostra para o python que é uma lista
+# O colchetes serve para mostra para o python que é uma lista
 print(nomes)
