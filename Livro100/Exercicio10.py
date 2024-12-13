3c32ff4d7ccb08d3a5036c6bd73980d1aa1bc4ef
GBJ008 <gabrielerik140@gmail.com>
2024-12-12 23:29:46 -0300

    Mais um exercicio do livro (Phyton na Prática)ensinado como usar os colchete e o len

--- Livro100/Exercicio10.py
+++ Livro100/Exercicio10.py
@@ -1,4 +1,6 @@
 nomes = ['Ana','Carlos','Daiane','Fernando','Maria']
-
+#        0       1         2       3         4      
+# len vai  ler o que esta ecrito na váriavel 
 print(len(nomes))
+# o numero 3 dentro do colchetes  contar 0,1,2,3 e escolher 
 print(nomes[3])
