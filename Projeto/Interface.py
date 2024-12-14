db0792b918e2563fb9a36f189d027e6754b83c3c
GBJ008 <gabrielerik140@gmail.com>
2024-12-13 23:49:16 -0300

    nesse projeto estou aprendendo a mecher no PySImpleGUI fazendo um interface simpre de login

---
+++ Projeto/Interface.py
@@ -0,0 +1,26 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+# Layout
+sg.theme('Reddit')
+layout = [
+    # entra de input onde o usuario ira colocar o seu dados para passa para proxima etapa
+    [sg.Text('Usuário'),sg.Input(key='Usuario',size=(20,1))],
+    
+    # senha do usuario para entra no seu login
+    [sg.Text('Senha '),sg.Input(key='Senha',password_char='*',size=(20,1))],
+    
+    # caixa vazia para selecionar se  você deseja que seja salvo =
+    [sg.Checkbox('Salvar o login')],
+    [sg.Button('Entrar')]
+]
+# Janela
+janela= sg.Window('Tela',layout)
+# Ler os Eventos
+while True:
+    # Manter a página aberta, e o break é para fechar a pag 
+    eventos, valores =  janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores ['Usuario'] == 'Gabriel' and valores ['Senha'] == '04012008': 
+            print('Seu login esta correto, seja Bem-vindo')
