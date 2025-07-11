134b0fe103e778cdc693f8b2ec24ef359dfba746
GBJ008 <gabrielerik140@gmail.com>
2025-07-11 15:29:56 -0300

    Delete Livro100 directory

--- Livro100/Exercicio8.py
+++
@@ -1,7 +0,0 @@
-numero1 = input('Digite um número: ')
-numero1= float(numero1)
-
-print(numero1)
-print(type(numero1))
-
-'''nesse exercício ele vai trasformar todo número em um número flutuante, ou seja um número com virgula'''
--- Livro100/Exercicio9.py
+++
@@ -1,3 +0,0 @@
-nomes = ['Ana','Carlos','Daiane','Fernando','Maria']
-# O colchetes serve para mostra para o python que é uma lista
-print(nomes)
